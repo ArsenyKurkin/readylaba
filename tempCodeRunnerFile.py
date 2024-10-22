2266e87fc2d75261168d5bedd8367aa375a32141
Arseny <kingstonaynes@gmail.com>
2024-10-22 23:48:31 +0300

    вот Артем Серебренников сделал вторую часть

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,6 +1,11 @@
-def summa(numbers):
+def oursum(numbers):
     return sum(numbers)
 
+def ourmax(numbers):
+    return max(numbers)
+
 numbers = [1, 2, 3, 4, 5]
-result = summa(numbers)
+result = oursum(numbers)
+print(result)
+result = ourmax(numbers)
 print(result)
